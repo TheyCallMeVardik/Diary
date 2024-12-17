97092e65d6726d096aab999329108c7e9d7dce59
Вардан Сафарян <vardansafarayn@gmail.com>
2024-12-17 14:56:28 +0300

    Delete Diary directory

--- Diary/Diary/Models/ApplicationUser .cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Diary.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string UserType {  get; set; }//тип пользователя 
-    }
-}
